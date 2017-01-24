91155d1b2f2c01d69a94a72d4d580f235e15367c
Katie Reed <katharynreedpierce@gmail.com>
2017-01-23 23:18:04 -0800

    reduce code to simple function

--- js/scripts.js
+++ js/scripts.js
@@ -13,50 +13,23 @@ var ireland = new Destination("Ireland", "2016", "Magners, Kev, and Johnny Cash"
 var portugal = new Destination("Portugal", "2009", "Lisbon at night");
 
 //user interface logic
-
 $(document).ready(function() {
-  $("button.spain").click(function(event) {
-    event.preventDefault();
-    $(".hidden-img").hide();
-    $(".place").text(spain.place);
-    $(".year").text(spain.year);
-    $(".memory").text(spain.memory);
-    $("#spain-img").show();
-  });
 
-  $("button.scotland").click(function(event) {
+  //this fires when ANY button is clicked
+  $("button").click(function(event) {
     event.preventDefault();
-    $(".hidden-img").hide();
-    $(".place").text(scotland.place);
-    $(".year").text(scotland.year);
-    $(".memory").text(scotland.memory);
-    $("#scotland-img").show();
-  });
 
-  $("button.portugal").click(function(event) {
-    event.preventDefault();
-    $(".hidden-img").hide();
-    $(".place").text(portugal.place);
-    $(".year").text(portugal.year);
-    $(".memory").text(portugal.memory);
-    $("#portugal-img").show();
-  });
+     //this grabs the button (event.target) and fetches the value of the attribute of 'data-name'
+    var countryName = $(event.target).attr('data-name');
 
-  $("button.ireland").click(function(event) {
-    event.preventDefault();
+    //window holds all local variables, even the ones defined above (spain, scotland, etc...)
+    //if window is fed in a string (countryName), it finds the variable that matches the name
+    //e.g. window['spain'] will return the variable of spain
+    var countryObject = window[countryName];
     $(".hidden-img").hide();
-    $(".place").text(ireland.place);
-    $(".year").text(ireland.year);
-    $(".memory").text(ireland.memory);
-    $("#ireland-img").show();
-  });
-
-  $("button.canada").click(function(event) {
-    event.preventDefault();
-    $(".hidden-img").hide();
-    $(".place").text(canada.place);
-    $(".year").text(canada.year);
-    $(".memory").text(canada.memory);
-    $("#canada-img").show();
+    $(".place").text(countryObject.place);
+    $(".year").text(countryObject.year);
+    $(".memory").text(countryObject.memory);
+    $("#" + countryName + "-img").show(); //simply uses the countryName string to show the right image
   });
 });
